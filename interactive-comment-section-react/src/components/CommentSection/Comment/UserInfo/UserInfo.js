eb23c30cb97df663bca64c062f7388517e4d1dac
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-07-29 19:54:25 +0200

    Create struct of user info in comments

---
+++ interactive-comment-section-react/src/components/CommentSection/Comment/UserInfo/UserInfo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./UserInfo.scss";
+
+export default function UserInfo({ user, createdAt }) {
+  return (
+    <div className="user-info-container">
+      <div className="user-info">
+        <img src={`${user.image.png}`} alt="profile" />
+        <h3>{user.username}</h3>
+        <span>{createdAt}</span>
+      </div>
+      <div className="reply-btn">
+        <img src={"./images/icon-reply.svg"} alt="reply" />
+        <span>Reply</span>
+      </div>
+    </div>
+  );
+}
