da34a02f4762e7bcefd50718b14ccba2a8f55302
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-07-31 17:30:01 +0200

    Add new comment component to struct

--- interactive-comment-section-react/src/components/CommentSection/CommentSection.js
+++ interactive-comment-section-react/src/components/CommentSection/CommentSection.js
@@ -1,14 +1,16 @@
 import React from "react";
+import AddComment from "../AddComment/AddComment";
 import Comment from "./Comment/Comment";
 import "./CommentSection.scss";
 
-export default function CommentSection({ comments }) {
+export default function CommentSection({ comments, currentUser }) {
   return (
     <div className="comment-section">
       {comments &&
         comments.map((el) => {
           return <Comment key={el.id} commentData={el} />;
         })}
+      <AddComment currentUser={currentUser} isReplying={false} />
     </div>
   );
 }
