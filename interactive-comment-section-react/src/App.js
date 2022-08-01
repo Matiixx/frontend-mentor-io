516c7c55f3f9ba465bb91c3772796c262f4b5779
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-08-01 17:34:37 +0200

    Add main function to App.js to update states

--- interactive-comment-section-react/src/App.js
+++ interactive-comment-section-react/src/App.js
@@ -3,6 +3,7 @@ import dataJSON from "./data.json";
 import { useEffect } from "react";
 import CommentSection from "./components/CommentSection/CommentSection";
 import { useLocalStorage } from "./utils/useLocalStorage";
+import { AddNewComment, GetLastID } from "./utils/utility";
 
 function App() {
   const [data, setData] = useLocalStorage("comments", {});
@@ -16,9 +17,17 @@ function App() {
     // eslint-disable-next-line
   }, []);
 
+  const addComment = (content) => {
+    AddNewComment(content, data.currentUser, [data, setData]);
+  };
+
   return (
     <div className="App">
-      <CommentSection comments={data.comments} currentUser={data.currentUser} />
+      <CommentSection
+        comments={data.comments}
+        currentUser={data.currentUser}
+        addCommentHandle={addComment}
+      />
     </div>
   );
 }
