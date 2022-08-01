60fe4e72568cbc4dc45c78674b0657948c12fd5b
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-08-01 17:18:16 +0200

    Basic utility functions
    Adding new comment to local storage, getting new id

---
+++ interactive-comment-section-react/src/utils/utility.js
@@ -0,0 +1,34 @@
+import { useLocalStorage } from "./useLocalStorage";
+
+function GetLastID(comments, maxID) {
+  for (let com of comments) {
+    if (com.id > maxID) maxID = com.id;
+    if (com?.replies && com.replies.length !== 0) {
+      let a = GetLastID(com.replies, maxID);
+      maxID = a > maxID ? a : maxID;
+    }
+  }
+  return maxID;
+}
+
+export function AddNewComment(content, user) {
+  let data = JSON.parse(localStorage.getItem("comments"));
+  // console.log(content, user);
+
+  let newID = GetLastID(data.comments, 0) + 1;
+  let newComment = {
+    content,
+    createdAt: "1 sec ago",
+    id: newID,
+    replies: [],
+    score: 0,
+    user,
+  };
+
+  let newData = {
+    comments: [...data.comments, newComment],
+    currentUser: data.currentUser,
+  };
+
+  localStorage.setItem("comments", JSON.stringify(newData));
+}
