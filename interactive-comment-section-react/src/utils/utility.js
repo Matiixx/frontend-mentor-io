087a97fa42cec245d685507ce10f90129d30c9fd
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-08-02 17:05:00 +0200

    Add reply recursively

--- interactive-comment-section-react/src/utils/utility.js
+++ interactive-comment-section-react/src/utils/utility.js
@@ -1,6 +1,4 @@
-// import { useLocalStorage } from "./useLocalStorage";
-
-function GetLastID(comments, maxID) {
+export function GetLastID(comments, maxID) {
   for (let com of comments) {
     if (com.id > maxID) maxID = com.id;
     if (com?.replies && com.replies.length !== 0) {
@@ -23,6 +21,18 @@ function GetCommentByID(comments, id) {
   return null;
 }
 
+function CreateNewComment(content, data) {
+  let newID = GetLastID(data.comments, 0) + 1;
+  return {
+    id: newID,
+    content,
+    createdAt: "1 sec ago",
+    replies: [],
+    score: 0,
+    user: data.currentUser,
+  };
+}
+
 function DeleteRecursive(comments, id) {
   return comments
     .map((el) => {
@@ -36,27 +46,42 @@ function DeleteRecursive(comments, id) {
     });
 }
 
-export function AddNewComment(content, user, [data, setData]) {
-  let newID = GetLastID(data.comments, 0) + 1;
-  let newComment = {
-    id: newID,
-    content,
-    createdAt: "1 sec ago",
-    replies: [],
-    score: 0,
-    user,
-  };
+function AddReplyRecursively(comments, parentID, reply) {
+  return comments.map((el) => {
+    if (el.id === parentID) {
+      if ("replies" in el) return { ...el, replies: [...el.replies, reply] };
+      return { ...el, replies: [reply] };
+    }
+    if ("replies" in el) {
+      el.replies = AddReplyRecursively(el.replies, parentID, reply);
+    }
+    return el;
+  });
+}
 
-  let newData = {
+export function AddNewComment(content, [data, setData]) {
+  const newComment = CreateNewComment(content, data);
+  setData({
     comments: [...data.comments, newComment],
     currentUser: data.currentUser,
-  };
-  setData(newData);
+  });
 }
 
 export function DeleteComment(commentID, [data, setData]) {
   setData({
     comments: DeleteRecursive(data.comments, commentID),
+    currentUser: data.currentUser,
+  });
+}
+
+export function AddNewReply(parentID, content, [data, setData]) {
+  const newReply = {
+    ...CreateNewComment(content, data),
+    replyingTo: GetCommentByID(data.comments, parentID).user.username,
+  };
+
+  setData({
+    comments: AddReplyRecursively(data.comments, parentID, newReply),
     currentUser: data.currentUser,
   });
 }
